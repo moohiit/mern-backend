bc1829904530c3f012030e7db4a18656e3ffa080
Mohit Patel <pmohit645@gmail.com>
2024-11-11 15:59:25 +0530

    Update server.js PORT update

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 })
 
 //listen the port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   connectDb();
   console.log(`Server is running on http://localhost:${PORT}`);
